da84b8c05fddcb608ecaed524087194336c4961d
Filipe Camarata <filipeedvandro942@gmail.com>
2023-10-21 02:04:00 +0100

    feat: Arquivos adicionais

---
+++ pequeno_challenge.php
@@ -0,0 +1,53 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pequeno-challenge</title>
+</head>
+<body>
+    <h1>Pequeno Challenge</h1>
+
+    <form action="challenge.php" method ="POST">
+    <p>
+        <label for="id_time1">Equipa1</label>
+        <input type="text" name="time1" id="id_time1">
+        <label for="id_pontos1">Pontos</label>
+        <input type="number" name="pontos1" id="id_pontos1">
+    </p>
+    <p>
+        <label for="id_time2">Equipa2</label>
+        <input type="text" name="time2" id="id_time2">
+        <label for="id_pontos2">Pontos</label>
+        <input type="number" name="pontos2" id="id_pontos2">
+    </p>
+    <p>
+        <label for="id_time3">Equipa3</label>
+        <input type="text" name="time3" id="id_time3">
+        <label for="id_pontos3">Pontos</label>
+        <input type="number" name="pontos3" id="id_pontos3">
+    </p>
+    <p>
+        <label for="id_time4">Equipa4</label>
+        <input type="text" name="time4" id="id_time4">
+        <label for="id_pontos4">Pontos</label>
+        <input type="number" name="pontos4" id="id_pontos4">
+    </p>
+    <p>
+        <label for="id_time5">Equipa5</label>
+        <input type="text" name="time5" id="id_time5">
+        <label for="id_pontos5">Pontos</label>
+        <input type="number" name="pontos5" id="id_pontos5">
+    </p>
+    <p>
+        <input type="submit" value="Enviar">
+    </p>
+
+
+
+
+    </form>
+    
+</body>
+</html>

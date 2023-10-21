813ca32589ac2cc343719ac344dacfcc4ea5ff6d
Filipe Camarata <filipeedvandro942@gmail.com>
2023-10-21 01:54:15 +0100

    feat: Adicionei o challenge

---
+++ challenge.php
@@ -0,0 +1,35 @@
+<?php
+
+function calcularPontos (){
+    $pontosFuturos = array(
+        'Real Madrid' => 30,
+        'Barcelona' => 25,
+        'Atletico de Madrid' => 20,
+        'Valencia' => 18,
+        'Girona' => 15,
+
+    );
+
+foreach($pontosFuturos as $clube => $pontos){
+    $pontosFuturos[$clube] = $pontos;
+}
+
+arsort($pontosFuturos);
+
+return $pontosFuturos;
+
+}
+
+$pontosFuturos = calcularPontos();
+
+foreach($pontosFuturos as $clube => $pontos){
+   if($pontos%2==0){
+       echo $clube . ": " . $pontos + 3 . " pts<br>";
+}else{
+    echo $clube . ": " . $pontos . " pts<br>";
+}
+ }
+
+ 
+
+ 

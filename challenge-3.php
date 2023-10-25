b9962475228de583360b2e91910b4e732315ce0c
Filipe Camarata <filipeedvandro942@gmail.com>
2023-10-25 22:34:56 +0100

    feat: Resolução do challenge 3

--- challenge-3.php
+++ challenge-3.php
@@ -1,20 +1,19 @@
 <?php
 
-function multiplosDe2(){
-    $multiplos = array(20, 21, 34, 3, 8, 40, 24, 2, 66, 0);
+$multiplos = array(20, 21, 34, 3, 8, 57, 24, 2, 66, 0);
 
-foreach($multiplos as $k => $int){
-    $multiplos[$k] = $int;
-
-}
-if($int % 2 == 0){
-       
-  echo $int ;     
-}
-
-arsort($multiplos);
+function multiplosDe2($multiplos){
+   
+rsort($multiplos);
+return $multiplos;
  
 }
 
-$multi = multiplosDe2();
-echo $multi;
+$multi = multiplosDe2($multiplos);
+
+foreach($multi as $v){
+    if($v % 2 == 0){
+        echo  $v . "<br>";
+    }
+    
+}

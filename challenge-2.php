be35ee1944bc2f30a0b9ef1953dd98ac1ace7f68
Filipe Camarata <filipeedvandro942@gmail.com>
2023-10-25 21:38:15 +0100

    feat. Actualização básica

--- challenge-2.php
+++ challenge-2.php
@@ -1,9 +1,9 @@
 <?php
 $clubes = ["Real Madrid", "Barcelona", "Atletico", "Girona", "Valencia"];
+$pontos_iniciais = [30, 21, 20, 16, 15];
 
-
-function actualizar_pontos($clubes){
-    $pontos_iniciais = [30, 21, 20, 16, 15];
+function actualizar_pontos($clubes, $pontos_iniciais){
+  
 
     for($i= 0; $i < count($clubes); $i++){
         if($pontos_iniciais[$i] % 2 == 0){
@@ -20,7 +20,7 @@ return $clubes_pontos;
 } 
 
 
-$clubes_pontos_actual = actualizar_pontos($clubes);
+$clubes_pontos_actual = actualizar_pontos($clubes, $pontos_iniciais);
 
 foreach ($clubes_pontos_actual as $clube => $pontos ){
     echo $clube . ": " . $pontos . " pts<br>";

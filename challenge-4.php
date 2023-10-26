8ae92f301d2b20716acc9e6299be4ceec9be7540
Filipe Camarata <filipeedvandro942@gmail.com>
2023-10-26 22:39:59 +0100

    feat: Actualização do challenge 4

--- challenge-4.php
+++ challenge-4.php
@@ -2,14 +2,75 @@
 
 $L = $_GET["valorL"];
 
-$A = 10;
-$B = 14.5;
-$C = 7;
+$a = 19;
+$b = 14.6;
+$c = 6;
 
 if($L == 1){
-    echo $C . "<br>" . $A . "<br>" . $B;
-}elseif($L == 2){
-    echo $B . "<br>" . $A . "<br>" . $C; 
-}elseif($L == 3){
-    echo $A . "<br>" . $B . "<br>" . $C;   
+    if($a > $b && $a > $c && $b > $c){
+        echo $c . "<br>" . $b . "<br>" . $a;
+
+    }elseif ($a > $b && $a > $c && $c > $b){
+        echo $b . "<br>" . $c . "<br>" . $a;
+    
+    }else if ( $b > $a && $b > $c && $a > $c){
+        echo $c . "<br>" . $a . "<br>" . $b;
+        
+    } else if ( $b > $a && $b > $c && $c > $a){
+        echo $a . "<br>" . $c . "<br>" . $b;
+       
+    }else if ( $c > $a && $c > $b && $b > $a){
+        echo $a . "<br>" . $b . "<br>" . $c;
+           
+    } else if ( $c > $a && $c > $b && $a > $b){
+        echo $b . "<br>" . $a . "<br>" . $c;
+        
+    }
 }
+
+if ($L == 2){
+    if ($a < $b && $a < $c && $b < $c){
+        echo $c . "<br>" . $b . "<br>" . $a;
+        
+    }else if ($a < $b && $a < $c && $c < $b){
+        echo $b . "<br>" . $c . "<br>" . $a;
+      
+    }else if ( $b < $a && $b < $c && $a < $c){
+        echo $c . "<br>" . $a . "<br>" . $b;
+       
+    }else if ( $b < $a && $b < $c && $c < $a){
+        echo $a . "<br>" . $c . "<br>" . $b;
+        
+    }else if ( $c < $a && $c < $b && $b < $a){
+        echo $a . "<br>" . $b . "<br>" . $c;
+        
+    }else if ( $c < $a && $c < $b && $a < $b){
+        echo $b . "<br>" . $a . "<br>" . $c;
+        
+    }
+}
+
+if ($L == 3){
+    if ($a < $b && $a < $c && $b < $c){
+        echo $b . "<br>" . $c . "<br>" . $a;
+      
+    }else if ($a < $b && $a < $c && $c < $b){
+        echo $c . "<br>" . $b . "<br>" . $a;
+       
+    }else if ( $b < $a && $b < $c && $a < $c){
+        echo $a . "<br>" . $c . "<br>" . $b;
+        
+    }else if ( $b < $a && $b < $c && $c < $a){
+        echo $c . "<br>" . $a . "<br>" . $b;
+       
+    }else if ( $c < $a && $c < $b && $b < $a){
+        echo $b . "<br>" . $a . "<br>" . $c;
+       
+    } else if ( $c < $a && $c < $b && $a < $b){
+        echo $a . "<br>" . $b . "<br>" . $c;
+        
+    }
+    return 0;
+}
+
+
